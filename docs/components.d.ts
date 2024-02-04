44ff9dc07a6cd9c89ef3e817219a04a3309a03dc
kohai <i@kohai.top>
2024-02-04 21:34:13 +0800

    chore: update action

--- docs/components.d.ts
+++ docs/components.d.ts
@@ -10,15 +10,7 @@ declare module 'vue' {
     ElOption: typeof import('element-plus/es')['ElOption']
     ElSelect: typeof import('element-plus/es')['ElSelect']
     ElSwitch: typeof import('element-plus/es')['ElSwitch']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
-    IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
-    IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
-    IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
-    IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
-    WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
   }
 }
--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -6,14 +6,23 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { version } from '../packages/vue/package.json'
 import mdLoader from './build/md-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line node/prefer-global/process
   const env = loadEnv(mode, process.cwd())
+  let base = env.VITE_BASE_URL
+  if (mode === 'gh-pages-ver')
+    // eslint-disable-next-line node/prefer-global/process
+    base = `${loadEnv('gh-pages', process.cwd()).VITE_BASE_URL}/${version}`
 
   return {
-    base: env.VITE_BASE_URL,
+    base,
+    build: {
+      outDir: mode === 'gh-pages-ver' ? `dist/${version}` : 'dist',
+    },
     plugins: [
       mdLoader(),
       vue({
